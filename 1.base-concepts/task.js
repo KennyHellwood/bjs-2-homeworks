5c6c76d015c3264ff4f3255f7a265b576e042c1b
KennyHellwood <ilya.zyablov@mail.ru>
2022-10-14 12:23:21 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let disc = Math.pow(b, 2) - 4 * a * c;
+  if (disc < 0) {
+    arr = [];
+  } else if (disc === 0) {
+    arr = [Number(-b / (2 * a).toFixed(2))];
+  } else if (disc > 0) {
+    let subArr = [];
+    subArr.push(Number(((-b + Math.sqrt(disc)) / (2 * a)).toFixed(2)));
+    subArr.push(Number(((-b - Math.sqrt(disc)) / (2 * a)).toFixed(2)));
+    arr = subArr;
+  }
   return arr; // array
 }
 
