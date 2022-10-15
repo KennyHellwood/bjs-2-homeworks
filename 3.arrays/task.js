64a1814f6e0e7cb8c170b1be1529ca5255ca60c7
KennyHellwood <ilya.zyablov@mail.ru>
2022-10-15 21:54:12 +0300

    third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+    return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+    return resultArr; // array
 }
