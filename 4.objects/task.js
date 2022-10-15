f729aadea5c3ffe6295bb8a764fc69831b1a022f
KennyHellwood <ilya.zyablov@mail.ru>
2022-10-16 02:30:41 +0300

    forth task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,32 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    marks.forEach(mark => this.addMark(mark));
+}
+
+Student.prototype.getAverage = function () {
+    const sum = this.marks.reduce((acc, mark) => acc + mark);
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
