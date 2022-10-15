336a905fed08fa50164b7e543611935b299dcc02
KennyHellwood <ilya.zyablov@mail.ru>
2022-10-15 15:17:59 +0300

    second task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,50 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+    let min = arr[arr.length-1];
+    let max = arr[arr.length-1];
+    let sum = 0;
+    let avg;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        sum += arr[i];
+    }
+    avg = Number((sum / arr.length).toFixed(2));
+    return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+    let sum = arr.reduce(
+        (previousValue, currentValue) => previousValue + currentValue
+    );
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
+  let subSum = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+      subSum = func(arrOfArr[i]);
+      if (subSum > max) {
+          max = subSum;
+      }
+  }
 
-  // Ваш кода
-  // for ...
-  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let min = arr.reduce(
+        (previousValue, currentValue) => Math.min(previousValue, currentValue)
+    );
+    let max = arr.reduce(
+        (previousValue, currentValue) => Math.max(previousValue, currentValue)
+    );
+    return Math.abs(max-min);
 }
